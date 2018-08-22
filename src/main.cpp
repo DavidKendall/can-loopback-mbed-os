ea4a9de3feed61e6539f634475c83def4f0769a9
David Kendall <david.kendall@northumbria.ac.uk>
2018-08-22 13:57:10 +0100

    Minor tidying up

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "mbed.h"
 #include "fsl_flexcan.h"
 
+#define TX_BUF_ID 8
+#define RX_BUF_ID 9
+
 Ticker ticker;
 DigitalOut green(LED_GREEN);
 Serial pc(USBTX, USBRX); // tx, rx
@@ -14,7 +17,7 @@ uint32_t counter = 0;
 void send(void) {
     counter += 1;
     txFrame.dataWord0 = counter;
-    if (kStatus_Success == FLEXCAN_TransferSendBlocking(CAN0, 0, &txFrame)) {
+    if (kStatus_Success == FLEXCAN_TransferSendBlocking(CAN0, TX_BUF_ID, &txFrame)) {
         pc.printf("TxOk: %09ld\n\r", counter);
         green = 1;
     } else {
@@ -25,11 +28,12 @@ void send(void) {
 int main() {
     FLEXCAN_GetDefaultConfig(&flexcanConfig);
     flexcanConfig.enableLoopBack = true;
-    FLEXCAN_Init(CAN0, &flexcanConfig, 50000000UL);
+    FLEXCAN_Init(CAN0, &flexcanConfig, CLOCK_GetOsc0ErClkFreq());
+    FLEXCAN_SetTxMbConfig(CAN0, TX_BUF_ID, true);
     mbconfig.type = kFLEXCAN_FrameTypeData;
     mbconfig.format = kFLEXCAN_FrameFormatStandard;
     mbconfig.id = FLEXCAN_ID_STD(0x123);
-    FLEXCAN_SetRxMbConfig(CAN0, 1, &mbconfig, true);
+    FLEXCAN_SetRxMbConfig(CAN0, RX_BUF_ID, &mbconfig, true);
     txFrame.length = 4;
     txFrame.type = kFLEXCAN_FrameTypeData; 
     txFrame.format = kFLEXCAN_FrameFormatStandard;
@@ -37,7 +41,7 @@ int main() {
     pc.printf("\n\rCAN Loopback Test\n\r");
     ticker.attach(send, 1);
     while(true) {
-        if (kStatus_Success == FLEXCAN_TransferReceiveBlocking(CAN0, 1, &rxFrame)) {
+        if (kStatus_Success == FLEXCAN_TransferReceiveBlocking(CAN0, RX_BUF_ID, &rxFrame)) {
             pc.printf("RxOk: %09ld\n\r", rxFrame.dataWord0);
             green = 0;
         } else {
